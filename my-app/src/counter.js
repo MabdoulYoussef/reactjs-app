c1c012626e708ffb8dce70c299cc615b1fb831c3
MabdoulYoussef <mabdoulyoussef@hotmail.com>
2025-01-12 17:50:30 +0100

    added counter plus

--- my-app/src/counter.js
+++ my-app/src/counter.js
@@ -8,7 +8,7 @@ const Counter = () => {
   const increment = () => {
     setCount(count + 1); // Zed 1 l-value dyal count
   };
-// error solved l3alamiya
+// error solv
   return (
     <div>
       <h1>Counter: {count}</h1>
