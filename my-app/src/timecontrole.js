43dc5273ca542f43cd37131ba8b72883f318ad46
MabdoulYoussef <mabdoulyoussef@hotmail.com>
2025-01-21 12:16:44 +0100

    file added

---
+++ my-app/src/timecontrole.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  // Declare state: counter starts at 0
+  const [count, setCount] = useState(0);
+
+  // Function to increment the counter
+  const increment = () => {
+    setCount(count + 1); // Zed 1 l-value dyal count
+  };
+  //solvedd
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={increment}>+1</button>
+    </div>
+  );
+};
+export default Counter;
