5e8423fc01696a5436c8a179c710246e787f7707
Mabdoul <mabdoulyoussef@hotmail.com>
2025-01-29 13:24:16 +0100

    learning new stuff

--- my-app/src/Counter.jsx
+++ my-app/src/Counter.jsx
@@ -1,20 +1,44 @@
+
 import { useState } from "react";
 
+function ToDoList() {
+    // L7ala dyal tasks: Liste li ghadi n7att fihom l'tasks
+    const [tasks, setTasks] = useState([]); 
 
-function Counter() {
-    const [Count , SetCount] = useState(0)
-    return (<div>
-        <p>Count : {Count}</p>
-        <button onClick={() => SetCount(Count + 1)}>Increment</button>
-        <button onClick={() => SetCount(Count - 1)}>Decrement</button><br />
-        <button onClick={() => SetCount(Count + 10)}>+ 10</button>
+    // L7ala dyal text: Li katmchi m3aha l'input
+    const [text, setText] = useState(''); 
 
-        <button onClick={() => SetCount(0)}>Reset</button>
-    </div>
-    )
+    // Fonction bach tzid task f l'array
+    const addTask = () => {
+            // Ziyada task jdida f l'array tasks
+            setTasks([...tasks, text]); 
+            setText("");
+    };
+
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <label >WRITE YOUR TASK</label>
+            {/* L'input dyal task, li kaytsajil fiha chi task */}
+            <input 
+                type="text" 
+                value={text}  // L'input ghadi yb9a dayman m3a text
+                onChange={(e) => setText(e.target.value)}  // Kima kaydkhl chi kalim f l'input, kaytbadl text
+                placeholder="Type your task here..." 
+                id="task-input"
+            />
+            {/* Button li kay3tik t7ell l'ajout de task */}
+            <button onClick={addTask}>Add Task</button>
+            
+            {/* Kay3rdo l'list dyal tasks */}
+            <ul>
+                {/* Katloop 3la l'array dyal tasks bach t7att kol task fi <li> */}
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
-export default Counter;
 
-
-
-
+export default ToDoList;
